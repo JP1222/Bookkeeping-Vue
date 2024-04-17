e64359fa1c78417471eee26d69450cf89d2d2a3b
JP1222 <130532438+JP1222@users.noreply.github.com>
2024-04-17 19:59:10 +0800

    Update update_project.sh

--- update_project.sh
+++ update_project.sh
@@ -4,8 +4,11 @@ git pull
 # 删除旧容器
 docker compose down
 
-# 清理旧版本的Docker镜像
-docker image prune -f
+# --build选项确保Docker Compose重新构建服务的镜像，会把旧镜像tag去掉
+docker compose up -d --build
+
+# 清理所有未使用的镜像，包括悬挂的和未被任何容器引用的镜像
+docker image prune -af
 
 # 启动新容器
 docker compose up -d
