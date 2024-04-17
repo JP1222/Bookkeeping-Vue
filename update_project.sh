62ec31512bd6788fdb2cc56df96575043f098a40
JP1222 <130532438+JP1222@users.noreply.github.com>
2024-04-17 19:51:00 +0800

    Update update_project.sh

--- update_project.sh
+++ update_project.sh
@@ -1,8 +1,11 @@
 # 从Git仓库拉取最新的项目代码
 git pull
 
-# 使用Docker Compose构建（重新构建改变的服务）并后台启动所有服务
-docker-compose up -d --build
+# 删除旧容器
+docker down
 
 # 清理旧版本的Docker镜像
 docker image prune -f
+
+# 启动新容器
+docker compose up -d
